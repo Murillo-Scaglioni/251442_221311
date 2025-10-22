7d5b2862cff6f6d8e283ffc95a629ad42d37ef40
Murillo-Scaglioni <John_lennon_91@hotmail.com>
2025-10-22 20:27:23 -0300

    Adicionar arquivos de projeto.

---
+++ 251442_221311/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _251442_221311
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
